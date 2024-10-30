a10962ecf8d99c2630c06af8a3e7896cb3113f46
dcipicchio7 <daultoncipicchio@gmail.com>
2024-10-30 19:39:47 -0400

    test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/about', pathMatch: 'full' },
+  { path: 'about', component: AboutMeComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+];
